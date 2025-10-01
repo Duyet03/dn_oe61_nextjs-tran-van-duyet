25a8d315632e51bdfd6c9abbfdcef5eaa4102874
Duyet <duyetvan03@gmail.com>
2025-10-01 11:30:02 +0700

    module 1

---
+++ src/app/(dashboard)/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function PostsPage() {
+  return (
+    <div>
+      <h1>Đây là trang Posts</h1>
+    </div>
+  );
+}
---
+++ src/app/components/Greeting.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Greeting() {
+  return (
+    <div>
+      <p>Hello from Client Component!</p>
+    </div>
+  );
+}
